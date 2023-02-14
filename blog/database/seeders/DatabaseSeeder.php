8ec733c2906924eaee16e7d59c8805ad4138b41a
OBrien-reece <indeche.evans.o@gmail.com>
2023-02-14 22:21:28 +0300

    DB seeding

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -19,22 +20,50 @@ class DatabaseSeeder extends Seeder
 
         User::truncate();
         Category::truncate();
+        Post::truncate();
 
-         User::factory(3)->create();
+         $user = User::factory()->create();
 
-         Category::create([
+         $personal = Category::create([
             'name' => 'Personal',
             'slug' => 'personal'
          ]);
 
-         Category::create([
+         $entertainment = Category::create([
             'name' => 'Entertainment',
             'slug' => 'entertainment'
          ]);
 
-         Category::create([
+         $work = Category::create([
             'name' => 'Work',
             'slug' => 'work'
+         ]);
+
+         Post::create([
+            'user_id' => $user->id,
+             'category_id' => $entertainment->id,
+             'title' => 'Entertainment title post',
+             'slug' => 'entertainment-slug',
+             'excerpt' => 'Entertainment post excerpt',
+             'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate eius eum hic provident unde? Accusantium aliquid corporis dignissimos illum inventore iste libero, magnam neque, perferendis repellendus rerum tempora temporibus voluptas.'
+         ]);
+
+         Post::create([
+            'user_id' => $user->id,
+             'category_id' => $personal->id,
+             'title' => 'Personal title post',
+             'slug' => 'personal-slug',
+             'excerpt' => 'Personal post excerpt',
+             'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate eius eum hic provident unde? Accusantium aliquid corporis dignissimos illum inventore iste libero, magnam neque, perferendis repellendus rerum tempora temporibus voluptas.'
+         ]);
+
+         Post::create([
+            'user_id' => $user->id,
+             'category_id' => $work->id,
+             'title' => 'Work title post',
+             'slug' => 'work-slug',
+             'excerpt' => 'Work post excerpt',
+             'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate eius eum hic provident unde? Accusantium aliquid corporis dignissimos illum inventore iste libero, magnam neque, perferendis repellendus rerum tempora temporibus voluptas.'
          ]);
 
         // \App\Models\User::factory()->create([
