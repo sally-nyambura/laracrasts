ff3cdeddb19115da34a65a9fb9cdd93a22caa677
OBrien-reece <indeche.evans.o@gmail.com>
2023-02-15 00:14:42 +0300

    Model $with constraint

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -9,6 +9,8 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $with = ['category', 'author'];
+
     public function category() {
         return $this->belongsTo(Category::class);
     }
